e6f1973b2e4c4044f303401acce270da20776ef6
Camilo Mason <camilomason@Camilos-MBP.home>
2019-04-06 09:12:23 -0400

    fixing model names and stuff

--- db/migrate/20190406131149_create_tvshows.rb
+++ db/migrate/20190406131149_create_tvshows.rb
@@ -2,13 +2,13 @@ class CreateTvshows < ActiveRecord::Migration[5.2]
   def change
     create_table :tvshows do |t|
       t.string :name
-      t.string :type
+      t.string :showtype
       t.integer :seasons
       t.date :startdate
       t.date :enddate
       t.string :creator
       t.string :photo
-      t.integer :episodecost_cents
+      t.integer :episodecost_dollars
 
       t.timestamps
     end
